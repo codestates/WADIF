a8bbf88af4abb8d3c3dcb1c2b4b199de9466f433
박민수 <pinion7rl@gmail.com>
2021-08-25 22:16:04 +0900

    server 환경세팅 #4

--- server/app.js
+++ server/app.js
@@ -5,14 +5,13 @@ const express = require("express");
 const app = express();
 
 // 2. 환경변수 세팅 (dotenv 모듈 설치)
-// const dotenv = require("dotenv");
-// dotenv.config();
+const dotenv = require("dotenv");
+dotenv.config();
 
 // 3. 분기별 라우팅 변수 선언
 const mainRouter = require("./routes/mainRouter");
 const userRouter = require("./routes/userRouter");
 const postRouter = require("./routes/postRouter");
-const indexRouter = require("./routes/indexRouter")
 
 // 4. 미들웨어 세팅
 // 4-1. cors는 보안을 잡기 위해 우선 디폴트로 세팅
@@ -26,7 +25,6 @@ app.use(express.urlencoded({ extended: true }));
 
 // 4-4. 토큰을 쿠키에 저장할 계획이기에 추가
 // 4-5. 라우터 세팅
-// app.use("/", indexRouter);
 app.use("/main", mainRouter);
 app.use("/users", userRouter);
 app.use("/posts", postRouter);
