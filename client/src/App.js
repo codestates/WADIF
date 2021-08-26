30e0cc814c92cbddaf574f5738f10f26dedc5e5e
vvsoggy <vvsogi@gmail.com>
2021-08-27 01:14:33 +0900

    [fix] 수정

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import "./App.css";
-import Nav from "./components/Nav/Nav";
 import { BrowserRouter, Route } from "react-router-dom";
 import Mainpage from "./pages/Mainpage";
 import Mypage from "./pages/Mypage";
@@ -7,13 +6,12 @@ import SecurityPage from "./pages/SecurityPage";
 import Positivepage from "./pages/Positivepage";
 import Negativepage from "./pages/Negativepage";
 import Allboardpage from "./pages/Allboardpage";
-// import SignUpAndSignIn from "./components/SignUpAndSignIn";
+import SignUpAndSignIn from "./components/SignUpAndSignIn";
 import CreatePost from "./components/CreatePost";
 
 function App() {
   return (
     <>
-      <Nav />
       <BrowserRouter>
         <Route exact path="/" component={Mainpage} />
         <Route path="/mypage" component={Mypage} />
@@ -22,6 +20,7 @@ function App() {
         <Route path="/positive" component={Positivepage} />
         <Route path="/negative" component={Negativepage} />
         <Route path="/Allboard" component={Allboardpage} />
+        <Route path="/login" component={SignUpAndSignIn} />
       </BrowserRouter>
     </>
   );
