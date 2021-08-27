48efb25f21111533e82197b87333d79890e49c03
박민수 <pinion7rl@gmail.com>
2021-08-27 14:24:16 +0900

    dev dev

--- server/controllers/ctrlFunctions/user.js
+++ server/controllers/ctrlFunctions/user.js
@@ -23,20 +23,5 @@ module.exports = {
   },
 
   signup: async (req, res) => {
-    const {userId, password, username, email} = req.body
-
-    try {
-      const [result, created] = await users.findOrCreate({
-        where: {userId},
-        defaults: {password,username,email}
-      })
-      if (created) {
-        res.status(201).send({"message": "와디프에 오신 것을 환영합니다."})
-      } else {
-        res.status(408).send({"message": "이미 가입된 아이디입니다."})
-      }
-    } catch (e) {
-      throw e
-    }
   }
 }
