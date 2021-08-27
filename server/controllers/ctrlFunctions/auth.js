d794cd5c92467df2dc7f448452bff6f07528937d
박민수 <pinion7rl@gmail.com>
2021-08-27 16:38:09 +0900

    [feature] 로그인, 로그아웃 api 컨트롤러 resolve #39

--- server/controllers/ctrlFunctions/auth.js
+++ server/controllers/ctrlFunctions/auth.js
@@ -1,4 +1,10 @@
-const { generateAccessToken, sendAccessToken, isAuthorized } = require('../tokenFunctions');
+const { 
+  generateAccessToken, 
+  sendAccessToken, 
+  isAuthorized, 
+  sendRefreshToken, 
+  generateRefreshToken 
+} = require('../tokenFunctions');
 
 module.exports = {
   signin: (req, res) => {
@@ -6,15 +12,24 @@ module.exports = {
     users.findOne({ where: { userId, password }})
     .then(data => {
       if (!data) {
-        res.status(401).send('유효하지 않은 아이디이거나 비밀번호가 일치하지 않습니다.');
+        res.status(401).send('아이디 혹은 비밀번호가 일치하지 않습니다.');
       }
       const payload = data.dataValues;
       const accessToken = generateAccessToken(payload);
+      const refreshToken = generateRefreshToken(payload);
+
       sendAccessToken(res, accessToken);
+      sendRefreshToken(res, refreshToken);
     })
   },
 
   signout: (req, res) => {
-    res.send('hello')    
+    const checkedData = isAuthorized(req);
+    if (!checkedData) {
+      res.status(401).send('유효하지 않은 사용자 입니다.');
+    }
+    else {
+      res.status(200).send('로그아웃에 성공하였습니다.');
+    }
   }
 }
