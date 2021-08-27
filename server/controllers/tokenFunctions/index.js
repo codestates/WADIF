a6d97653ba1e1cc78a27bcbb068f3d38a6d20633
박민수 <pinion7rl@gmail.com>
2021-08-27 15:13:47 +0900

    [feature] tokenFunc progress#1

--- server/controllers/tokenFunctions/index.js
+++ server/controllers/tokenFunctions/index.js
@@ -2,11 +2,24 @@ const { sign, verify } = require('jsonwebtoken');
 
 module.exports = {
   generateAccessToken: (data) => {
+    return sign(data, process.env.ACCESS_SECRET, { expiresIn: "5m" });
   },
 
   sendAccessToken: (res, accessToken) => {
+    res.json({ data: { accessToken }, message: "ok" });
   },
   
   isAuthorized: (req) => {
+    const authorization = req.headers["authorization"];
+    if (!authorization) {
+      return null;
+    }
+    const token = authorization.split(" ")[1];
+    try {
+      return verify(token, process.env.ACCESS_SECRET);
+    } 
+    catch (err) {
+      return null;
+    }
   }
 };
