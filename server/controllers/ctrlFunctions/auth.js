e17add1f604925cc38ce7759a32338835713d9d8
박민수 <pinion7rl@gmail.com>
2021-08-27 14:44:11 +0900

    [feature] 로그인 구현 resolve #39

--- server/controllers/ctrlFunctions/auth.js
+++ server/controllers/ctrlFunctions/auth.js
@@ -2,7 +2,16 @@ const { generateAccessToken, sendAccessToken, isAuthorized } = require('../token
 
 module.exports = {
   signin: (req, res) => {
-    res.send('hello')
+    const { userId, password } = req.body;
+    users.findOne({ where: { userId, password }})
+    .then(data => {
+      if (!data) {
+        res.status(401).send('유효하지 않은 아이디이거나 비밀번호가 일치하지 않습니다.');
+      }
+      const payload = data.dataValues;
+      const accessToken = generateAccessToken(payload);
+      sendAccessToken(res, accessToken);
+    })
   },
 
   signout: (req, res) => {
