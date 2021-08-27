a464acc2ed616691b6ef42a839b4bcacfd2cdf44
Freetargeter <supreme5876@gmail.com>
2021-08-27 13:25:08 +0900

    [feat]debate 수정

--- client/src/components/Comment.jsx
+++ client/src/components/Comment.jsx
@@ -5,7 +5,8 @@ const Comment = () => {
 
   const CommentContainer = styled.div`
     width: 100%;
-    background: #f5f5f5;
+    display: flex;
+    flex-direction: column;
   `;
 
   const ProfileIcon = styled(Man)`
@@ -23,6 +24,7 @@ const Comment = () => {
   `;
 
   const UserInfo = styled(basicDiv)`
+    padding-top: 0.5em;
     margin-bottom: 0;
     padding-bottom: 0;
     .profileIcon {
@@ -31,17 +33,21 @@ const Comment = () => {
       background-color: #c7c7c7;
       border-radius: 50%;
       position: absolute;
-      top: 0.5em;
-      left: 0.5em;
+      top: 0em;
+      left: 0em;
       overflow: hidden;
       margin: auto;
     }
     .profile {
       position: relative;
-      top: -4.5em;
-      left: 4em;
+      top: -1.5em;
+      left: 2.2em;
       font-size: 5px;
     }
+  `;
+  const CommentText = styled.div`
+    width: 100%;
+    font-size: 13px;
   `;
 
   return (
@@ -55,7 +61,9 @@ const Comment = () => {
           <span>2020년 1월 1일</span>
         </div>
       </UserInfo>
-      
+      <CommentText>
+        <span>영국 배긴턴에 사는 앨리슨 웹은 최근 한 통의 연락을 받았다. 이름을 밝힐 수 없는 VIP(Very Important Person)가 탄 헬기가 그의 정원 뒷마당에 착륙해도 괜찮으냐는 요청이었다. 인근의 공항이 폐쇄돼 착륙할 곳이 없다는 이유에서다.</span>
+      </CommentText>
     </CommentContainer>
   )
 };
