ef76ea476a9c178834e0bc873848cba7d980921d
tjdgns5272 <69573484+tjdgns5272@users.noreply.github.com>
2021-08-30 17:03:58 +0900

    [feat] api 컨트롤러 작성 resolve #62

--- server/controllers/ctrlFunctions/main.js
+++ server/controllers/ctrlFunctions/main.js
@@ -3,9 +3,31 @@ const {
   sendAccessToken,
   isAuthorized,
 } = require('../tokenFunctions');
-
+const { posts, users } = require('../../models');
 module.exports = {
-  hotTopic: (req, res) => {
-    res.send('hello');
+  hotTopic: async (req, res) => {
+    console.log(req.query);
+    try {
+      const postData = await posts.findAll({
+        order: [['views', 'DESC']],
+        include: [
+          {
+            model: users,
+            attributes: ['username'],
+          },
+        ],
+      });
+      res.status(200).json({
+        data: postData.map((el) => {
+          el.dataValues.username = el.dataValues.user.username;
+          delete el.dataValues.user;
+          return el;
+        }),
+        message: '조회수 정렬로 게시글을 조회하였습니다.',
+      });
+    } catch (e) {
+      res.send('error');
+      throw e;
+    }
   },
 };
--- server/middlewares/authChecker.js
+++ server/middlewares/authChecker.js
@@ -20,33 +20,36 @@ module.exports = {
     const accessToken = authorization.split(' ')[1];
     const refreshToken = req.cookies.refreshToken;
 
-    console.log(accessToken);
-
     const accessTokenData = verify(
       accessToken,
       process.env.ACCESS_SECRET,
       (err, data) => {
         if (err) {
-          res.json({
-            data: null,
-            message: 'invalid refresh token, please log in again',
-          });
+          return null;
         } else {
           return data;
         }
       },
     );
-    console.log('asdfasdf, ', accessTokenData);
-    const refreshTokenData = verify(refreshToken, process.env.REFRESH_SECRET);
-    console.log('2222');
+    const refreshTokenData = verify(
+      refreshToken,
+      process.env.REFRESH_SECRET,
+      (err, data) => {
+        if (err) {
+          return null;
+        } else {
+          return data;
+        }
+      },
+    );
     if (!accessTokenData) {
-      if (refreshTokenData === undefined) {
+      if (!refreshTokenData) {
         // case1 : access, refresh 모두 만료됬을 경우
         res.json({
           data: null,
-          user: {},
           message: '토큰이 모두 만료 되었으니, 다시 로그인해주세요',
         });
+        res.redirect('/');
       } else {
         //case2: aceess는 만료됐지만, refresh는 유효한 경우
         const { userId } = refreshTokenData;
@@ -55,19 +58,25 @@ module.exports = {
           if (!data) {
             return res.json({
               data: null,
-              message: 'refresh token has been tempered',
+              message: '일치하는 유저 정보가 없습니다.',
             });
           }
           delete data.dataValues.password;
           const payload = data.dataValues;
 
           const newAccessToken = generateAccessToken(payload);
-          resendAccessToken(res, newAccessToken, payload);
+          req.body = {
+            data: { accessToken: newAccessToken, userInfo: payload },
+          };
+          // res.setHeader({ authorization: `Bearer ${newAccessToken}` });
+          // resendAccessToken(res, newAccessToken, payload);
           next();
         });
       }
     } else {
       // case4: accesss token과 refresh token 모두가 유효한 경우
+      req.body.userInfo = accessTokenData;
+      req.body.accessToken = accessToken;
       next();
     }
   },
